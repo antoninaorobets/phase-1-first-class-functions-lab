fc7e0057022f47df2158aed2a677d81deeea3012
antoninaorobets <pani.antonina.don@gmail.com>
2021-11-23 22:08:09 -0500

    commit tree 834595ef33b6212beb61f7ecab0f1f813944a0fc

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2)
+}
+
+
+const selectingDrivers =  [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(number) {
+   const farefunction = function (fare) { 
+       if (number === 4)  return fare*4;
+       else  return fare*fare;
+    }
+    return farefunction;
+}
+
+const fareDoubler = function(fare) {
+   return createFareMultiplier(4)(fare/2);
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(4)(fare/4*3);
+}
+
+
+
+function selectDifferentDrivers(drivers,cutDrivers){
+    return cutDrivers(drivers);
+} 
